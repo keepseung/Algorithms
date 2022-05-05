8e9583c805b17ec19c12beebb42f28110983bf52
keepseung <keepseung@gmail.com>
2022-05-06 01:01:15 +0900

    구간합, 소수판별 추가함

--- ThisIsCodingTest/11-other-algorithm/소수판별-에라토스테네스체.py
+++ ThisIsCodingTest/11-other-algorithm/소수판별-에라토스테네스체.py
@@ -1,15 +1,19 @@
 import math
 n = 1000 # 2부터 1000까지의 모든 수에 대하여 소수 판별
-
 #처음엔 모든 수가 소수(True) 인것으로 초기화(0과 1은 제외)
-# 소수 판별 함수 
-def is_prime_number(x):
-    # 2부터 x의 제곱근까지의 모든 수를 확인하며
-    for i in range(2, int(math.sqrt(x)) +1 ):
-        # x가 해당 수로 나누어떨어진다면
-        if x % i == 0:
-            return False # 소수가 아님
-    return True
+array = [True for i in range(n+1)]
 
-print(is_prime_number(4))
-print(is_prime_number(int(1e9)+1))
+# 에라토스테네스 채 알고리즘 수행
+# 2부터 n의 제곱근까지의 모든 수를 확인하며
+for i in range(2, int(math.sqrt(n))+1):
+    if array[i] == True: # i가 소수인 경우
+        # i를 제외한 i의 모든 배수를 지우기
+        j = 2 
+        while i*j <=n:
+            array[i*j] = False
+            j+=1
+
+# 모든 소수 출력
+for i in range(2,n+1):
+    if array[i]:
+        print(i, end=" ")
